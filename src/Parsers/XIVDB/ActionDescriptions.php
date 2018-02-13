887ac9f50536771f1743f878ef451d23695c631b
Josh Freeman <josh.freeman@sykescottages.co.uk>
2018-02-13 18:33:50 +0000

    More craziness to parse json

--- src/Parsers/XIVDB/ActionDescriptions.php
+++ src/Parsers/XIVDB/ActionDescriptions.php
@@ -27,7 +27,7 @@ class ActionDescriptions extends ParseHandler implements ParseInterface
 
             $this->output->writeln("Action: {$id}");
 
-            if ($id == 27) {
+            if ($id == 182) {
                 $description = $this->formatDescription($desc['Description']);
 
                 die("\n\n\n\n");
@@ -42,66 +42,34 @@ class ActionDescriptions extends ParseHandler implements ParseInterface
 
     private function formatDescription(string $description)
     {
-        $lines = [];
-        foreach(explode("\n", $description) as $line) {
-            $lines[] = $this->formatDescriptionCleaner(trim($line));
-        }
+        // fix colours
+        $description = $this->formatColor($description);
 
-        $lines = '<p>'. implode("</p><p>", $lines) .'</p>';
+        // build text trees
+        $adc = new ActionDescriptionsConditions();
+        $description = $adc->parse($description);
 
-        print_r($lines);
-    }
-
-    private function formatDescriptionCleaner($line)
-    {
-        /**
-         * These conditions are quite hacky but they help make things easier.
-         *
-         * Types: https://github.com/Rogueadyn/SaintCoinach/blob/f969b441584688c02dde2fadac548c4a5aaa3faa/SaintCoinach/Text/DecodeExpressionType.cs
-         *
-         *  GreaterThanOrEqualTo
-         *  UnknownComparisonE1
-         *  LessThanOrEqualTo
-         *  NotEqual
-         *  Equal
-         *
-         *  IntegerParameter
-         *  PlayerParameter
-         *  StringParameter
-         *  ObjectParameter
-         */
-
-        $conditions = [
-            '<Color(' => 'formatColor',
-        ];
-
-        foreach($conditions as $needle => $function) {
-            if (stripos($line, $needle) !== false) {
-                $line = $this->$function($line);
-            }
-        }
-
-        return $line;
+        return $description;
     }
 
     /**
      * Replace color entries with hex value
      */
-    private function formatColor($line)
+    private function formatColor($description)
     {
         // easy one
-        $line = str_ireplace('</Color>', '</span>', $line);
+        $description = str_ireplace('</Color>', '[END_SPAN]', $description);
 
         // replace all colour entries with hex values
-        preg_match_all("#<Color(.*?)>#is", $line, $matches);
+        preg_match_all("#<Color(.*?)>#is", $description, $matches);
 
         foreach($matches[1] as $number) {
             $number = filter_var($number, FILTER_SANITIZE_NUMBER_INT);
             $hex = substr(str_pad(dechex($number), 6, '0', STR_PAD_LEFT), -6);
 
-            $line = str_ireplace("<Color({$number})>", "<span style=\"color:#{$hex};\">", $line);
+            $description = str_ireplace("<Color({$number})>", "[START_SPAN style=\"color:#{$hex};\"]", $description);
         }
 
-        return $line;
+        return $description;
     }
 }
