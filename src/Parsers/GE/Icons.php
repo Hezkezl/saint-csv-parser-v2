2ed32927d4cf5e1399acbdccaef4866f064b30a6
Hezkezl <raistlindlleader@hotmail.com>
2018-11-16 17:55:16 -0700

    Fixed Icon parser (and updated name save to GE standard)

--- src/Parsers/GE/Icons.php
+++ src/Parsers/GE/Icons.php
@@ -13,25 +13,28 @@ class Icons implements ParseInterface
     use CsvParseTrait;
 
     public function parse()
-$saveto = __DIR__.'/icons';
-$url = 'https://xivapi.com/item?key=33417de77d63415ab211143e&limit=1000&columns=ID,Name&page=%s';
-@mkdir($saveto);
+    {
+        $saveto = __DIR__.'/icons';
+        $url = 'https://xivapi.com/item?key=33417de77d63415ab211143e&limit=1000&columns=ID,Name&page=%s';
+        @mkdir($saveto);
 
-foreach(range(1, 25) as $page) {
-    // grab all items
-    $items = json_decode(file_get_contents(sprintf($url, $page)));
+        foreach(range(1, 25) as $page) {
+            // grab all items
+            $items = json_decode(file_get_contents(sprintf($url, $page)));
 
-    foreach ($items->Results as $i => $item) {
-        $filename = "{$saveto}/Icon_{$item->Name}.png";
+            foreach ($items->Results as $i => $item) {
+                $filename = "{$saveto}/{$item->Name}_Icon.png";
 
-        // skip if we already have it
-        if (file_exists($filename)) {
-            continue;
+                // skip if we already have it
+                if (file_exists($filename)) {
+                    continue;
+                }
+
+                // copy file (if it doesn't exist, it wont copy)
+                $i++;
+                @copy("https://xivapi.com/i2/{$item->ID}.png", $filename);
+                echo "Page: {$page} ({$i} / {$items->Pagination->Results}) - {$item->Name}\n";
+            }
         }
-
-        // copy file (if it doesn't exist, it wont copy)
-        $i++;
-        @copy("https://xivapi.com/i2/{$item->ID}.png", $filename);
-        echo "Page: {$page} ({$i} / {$items->Pagination->Results}) - {$item->Name}\n";
     }
 }
