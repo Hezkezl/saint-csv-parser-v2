0c6e93fc07f7cbbcf411ec9c5ecf581100d539af
Hezkezl <raistlindlleader@hotmail.com>
2019-02-04 09:51:03 -0700

    Recipes.php completed!

--- src/Parsers/GE/Recipes.php
+++ src/Parsers/GE/Recipes.php
@@ -17,16 +17,15 @@ class Recipes implements ParseInterface
     const WIKI_FORMAT = 'http://ffxiv.gamerescape.com/wiki/{result}/Recipe?action=edit
 {{ARR Infobox Recipe
 |Recipe ID           = {index}
-|Result              = {result}
-|Result Count        = {resultcount}{unlockbook}{specialist}
+|Result              = {result}{resultcount}{unlockbook}{specialist}
 |Primary Skill       = {skill}
 |Primary Skill Level = {level}
+|Recipe Level        = {recipelevel}
 |Durability          = {durability}
 |Difficulty          = {difficulty}
 |Quality             = {quality}{requiredcrafts}{requiredcontrol}
-|Quick Synthesis     = {quicksynth}{quicksynthcrafts}{quicksynthcontrol}{status}{aspect}
-|Ingredient 1        = 
-|Ingredient 1 Amount = 
+|Quick Synthesis     = {quicksynth}{quicksynthcrafts}{quicksynthcontrol}{status}{aspect}{ingredient1}{ingredient2}
+{ingredients}
 }}';
 
     public function parse()
@@ -74,23 +73,31 @@ class Recipes implements ParseInterface
             $level = $RecipeLevelCsv->at($recipe['RecipeLevelTable'])['ClassJobLevel'];
             $star = str_repeat("{{Star}}", $RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Stars']);
             $levelstar = "$level $star";
-            $suggestedcrafts = $RecipeLevelCsv->at($recipe['RecipeLevelTable'])['SuggestedCraftsmanship'];
-            $difficulty = floor($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Difficulty']*($recipe['DifficultyFactor']/100));
-            $quality = floor($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Quality']*($recipe['QualityFactor']/100));
-            $durability = floor($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Durability']*($recipe['DurabilityFactor']/100));
+            //$suggestedcrafts = $RecipeLevelCsv->at($recipe['RecipeLevelTable'])['SuggestedCraftsmanship'];
+
+            // ingredient info
+            $Ingredients = [];
+            foreach(range(0,7) as $i) {
+                if(!empty($recipe["Item{Ingredient}[$i]"])) {
+                    $Ingredients[] = "|Ingredient ". ($i+3) ."        = ". $ItemCsv->at($recipe["Item{Ingredient}[$i]"])['Name'];
+                    $Ingredients[] .= "|Ingredient ". ($i+3) ." Amount = ". $recipe["Amount{Ingredient}[$i]"];
+                }
+            }
+            $Ingredients = implode("\n", $Ingredients);
 
             // Save some data
             $data = [
                 '{index}' => $recipe['id'],
                 '{result}' => $ItemCsv->at($recipe['Item{Result}'])['Name'],
-                '{resultcount}' => $recipe['Amount{Result}'],
+                '{resultcount}' => ($recipe['Amount{Result}'] > 1) ? "\n|Result Count        = ". $recipe['Amount{Result}'] : "",
                 '{unlockbook}' => ($recipe['SecretRecipeBook'] > 0) ? "\n|Acquired            = ". $RecipeSecretCsv->at($recipe['SecretRecipeBook'])['Name'] : "",
                 '{specialist}' => ($recipe['IsSpecializationRequired'] == "True") ? "\n|Specialist Only     = Yes" : "",
                 '{skill}' => $skill[$recipe['CraftType']],
                 '{level}' => ($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Stars'] > 0) ? $levelstar :  $level,
-                '{durability}' => $durability,
-                '{difficulty}' => $difficulty,
-                '{quality}' => $quality,
+                '{recipelevel}' => $recipe['RecipeLevelTable'],
+                '{durability}' => floor($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Durability']*($recipe['DurabilityFactor']/100)),
+                '{difficulty}' => floor($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Difficulty']*($recipe['DifficultyFactor']/100)),
+                '{quality}' => floor($RecipeLevelCsv->at($recipe['RecipeLevelTable'])['Quality']*($recipe['QualityFactor']/100)),
                 '{status}' => ($recipe['Status{Required}'] > 0) ? "\n|Status Required     = ". $StatusCsv->at($recipe['Status{Required}'])['Name'] : "",
                 '{aspect}' => ($recipe['RecipeElement'] > 0) ? "\n|Aspect              = ". $aspect[$recipe['RecipeElement']] : "",
                 '{requiredcrafts}' => ($recipe['RequiredCraftsmanship'] > 0) ? "\n|Craftsmanship Required = ". $recipe['RequiredCraftsmanship'] : "",
@@ -98,6 +105,9 @@ class Recipes implements ParseInterface
                 '{quicksynth}' => ($recipe['CanQuickSynth'] == "True") ? 'Yes' : 'No',
                 '{quicksynthcrafts}' => ($recipe['QuickSynthCraftsmanship'] > 0) ? "\n|Quick Synthesis Craftsmanship = ". $recipe['QuickSynthCraftsmanship'] : "",
                 '{quicksynthcontrol}' => ($recipe['QuickSynthControl'] > 0) ? "\n|Quick Synthesis Control = ". $recipe['QuickSynthControl'] : "",
+                '{ingredient1}' => "\n|Ingredient 1        = ". $ItemCsv->at($recipe['Item{Ingredient}[8]'])['Name'] ."\n|Ingredient 1 Amount = ". $recipe['Amount{Ingredient}[8]'],
+                '{ingredient2}' => ($recipe['Item{Ingredient}[9]'] > 0) ? "\n|Ingredient 2        = ". $ItemCsv->at($recipe['Item{Ingredient}[9]'])['Name'] ."\n|Ingredient 2 Amount = ". $recipe['Amount{Ingredient}[9]'] : "",
+                '{ingredients}' => $Ingredients,
             ];
 
             // format using Gamer Escape formatter and add to data array
