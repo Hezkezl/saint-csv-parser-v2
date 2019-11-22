795beb8cd1a82e527e17746e96aa06c9795c3cd8
Hezkezl <raistlindlleader@hotmail.com>
2019-11-22 12:39:17 -0700

    Update Leves.php

--- src/Parsers/GE/Leves.php
+++ src/Parsers/GE/Leves.php
@@ -14,24 +14,21 @@ class Leves implements ParseInterface
     use CsvParseTrait;
 
     // the wiki output format / template we shall use
-    const WIKI_FORMAT = "{{-start-}}
-'''{result}'''
-{{ARR Infobox Levequest
+    const WIKI_FORMAT = "{Top}{{ARR Infobox Levequest
 |Name  = {name}
 |Patch = {patch}
 |Level = {level}
 
 |Guildleve Type     = {guildtype}
-|Levequest Type     = {levetype}
-|Leve Duration      = 20m
+|Levequest Type     = {levetype}{duration}
 |Levequest Location ={grandcompany}
 
 <!-- Disciples of Magic, Disciples of War for \"Battlecraft\" leves. The full name of the class
 otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
-|Recommended Classes =  {classes}
+|Recommended Classes = {classes}
 
 |Objectives = <!-- Just list them exactly as they appear on screen -->
-{objective}
+{objective}{mobobjective}
 
 |Description = {description}
 
@@ -55,7 +52,7 @@ otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
 |Client = {client}
 
 |NPCs Involved  = {npcinvolve} <!-- List of NPCs involved (besides the quest giver,) comma separated-->
-|Mobs Involved  =  <!-- List any Mobs who are involved, comma separated-->
+|Mobs Involved  = {mobinvolve} <!-- List any Mobs who are involved, comma separated-->
 |Items Involved = {item} <!-- List any items used, comma separated-->
 |Wanted Target  =  <!-- Usually found during Battlecraft leves -->
 
@@ -65,7 +62,7 @@ otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
 |Etymology =
 |Images =
 |Notes =
-}}{{-stop-}}";
+}}{Bottom}";
 
     public function parse()
     {
@@ -76,12 +73,15 @@ otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
         $ItemCsv = $this->csv('Item');
         $CraftLeveCsv = $this->csv('CraftLeve');
         $GatheringLeveCsv = $this->csv('GatheringLeve');
+        $BattleLeveCsv = $this->csv('BattleLeve');
+        $BNpcNameCsv = $this->csv('BNpcName');
         $LevelCsv = $this->csv('Level');
         $MapCsv = $this->csv('Map');
         $PlaceNameCsv = $this->csv('PlaceName');
         $ClassJobCsv = $this->csv('ClassJobCategory');
         $JournalGenreCsv = $this->csv('JournalGenre');
         $ENpcResidentCsv = $this->csv('ENpcResident');
+        $EventItemCsv = $this->csv('EventItem');
 
         // (optional) start a progress bar
         $this->io->progressStart($LeveCsv->total);
@@ -92,10 +92,24 @@ otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
 
             $patch = '5.11';
 
-            // skip ones without a name
-            if (empty($leve['Name'])) {
+            // if I want to use pywikibot to create these pages, this should be true. Otherwise if I want to create pages
+            // manually, set to false
+            $Bot = "false";
+            $Name = $leve['Name'];
+
+            // skip ones without a name, and skip if name contains Kanji, Hiragana, or Katakana
+            if ((empty($Name)) || (preg_match("/[\x{30A0}-\x{30FF}\x{3040}-\x{309F}\x{4E00}-\x{9FBF}]+/u", $Name))) {
                 continue;
             }
+
+            // change the top and bottom code depending on if I want to bot the pages up or not
+            if ($Bot == "true") {
+                $Top = "{{-start-}}\n'''$Name/Patch'''\n$patch\n<noinclude>[[Category:Patch Subpages]]</noinclude>\n{{-stop-}}{{-start-}}\n'''$Name'''\n";
+                $Bottom = "{{-stop-}}";
+            } else {
+                $Top = "http://ffxiv.gamerescape.com/wiki/$Name?action=edit\n";
+                $Bottom = "";
+            };
 
             // Change the 'Levequest Type' parameter to appropriate name
             $levetype = false;
@@ -192,9 +206,15 @@ otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
             }
 
             // Objective text for Disciple of the Hand leves
-            $Objective = false;
+            $TradecraftObjective = false;
             $Item = false;
-            if ($leve['DataId'] >= 917054 && $leve['DataId'] <= 920000) {
+            $NpcInvolvement = false;
+            $Npc = false;
+            $MobInvolvement = [];
+            $InvolvementObjective = [];
+            $BattlecraftItemsInvolved = [];
+
+            if ($levetype == "Tradecraft") {
                 $CraftLeveItem = $CraftLeveCsv->at($leve['DataId'])['Item[0]'];
                 $CraftLeveItemQty = $CraftLeveCsv->at($leve['DataId'])['ItemCount[0]'];
                 $ItemSingle = $ItemCsv->at($CraftLeveItem)['Singular'];
@@ -202,37 +222,66 @@ otherwise (Fisher, Botanist, Goldsmith, Alchemist, etc) -->
                 $ItemVowel = $ItemCsv->at($CraftLeveItem)['StartsWithVowel'];
                 $Item = $ItemCsv->at($CraftLeveItem)['Name'];
                 $NpcName = $ENpcResidentCsv->at($LevelCsv->at($leve['Level{Levemete}'])['Object'])['Singular'];
-                if (($ItemVowel == 0 || $ItemVowel == 1) && $CraftLeveItemQty > 1) {
-                    $Objective = "*Deliver [[$Item|$ItemPlural]] to {{NPCLink|$NpcName}}. 0/$CraftLeveItemQty";
-                } elseif ($ItemVowel == 0 && $CraftLeveItemQty == 1) {
-                    $Objective = "*Deliver a [[$Item|$ItemPlural]] to {{NPCLink|$NpcName}}. 0/$CraftLeveItemQty";
-                } elseif ($ItemVowel == 1 && $CraftLeveItemQty == 1) {
-                    $Objective = "*Deliver an [[$Item|$ItemSingle]] to {{NPCLink|$NpcName}}. 0/$CraftLeveItemQty";
+                if ($CraftLeveItemQty > 1) {
+                    $TradecraftObjective = "*Deliver [[$Item|$ItemPlural]] to {{NPCLink|$NpcName}}. 0/$CraftLeveItemQty";
+                } elseif ($ItemVowel == "0" && $CraftLeveItemQty == "1") {
+                    $TradecraftObjective = "*Deliver a [[$Item|$ItemSingle]] to {{NPCLink|$NpcName}}. 0/$CraftLeveItemQty";
+                } elseif ($ItemVowel == "1" && $CraftLeveItemQty == "1") {
+                    $TradecraftObjective = "*Deliver an [[$Item|$ItemSingle]] to {{NPCLink|$NpcName}}. 0/$CraftLeveItemQty";
                 }
-            }
+            } elseif ($levetype == "Battlecraft") {
+                foreach(range(0,7) as $i) {
+                    if ($BattleLeveCsv->at($leve['DataId'])["BNpcName[$i]"] > 1 && $BattleLeveCsv->at($leve['DataId'])["ItemsInvolved[$i]"] == "0") {
+                        $BNpcName = ucwords(strtolower($BNpcNameCsv->at($BattleLeveCsv->at($leve['DataId'])["BNpcName[$i]"])['Singular']));
+                        $MobInvolvement[] = $BNpcName;
+                        $InvolvementObjective[0] = "*Defeat target enemies.";
+                        $InvolvementObjective[] = $BNpcName;
+                    }
+                    // doesn't work. attempt to make it so that if there's an item that appears during a battle leve, then it should get priority over
+                    // just displaying the "Defeat target enemies." text. But... doesn't work. Also need to finish adding in the item list for
+                    // items involved here, as it doesn't work either (items involved currently only works for tradecraft leves, but battlecraft
+                    // can have them too. They're just in the battleleve file instead of the craftleve file...)
+                    //if ($BattleLeveCsv->at($leve['DataId'])["BNpcName[$i]"] > 1 && $BattleLeveCsv->at($leve['DataId'])["ItemsInvolved[$i]"] > 1) {
+                        //$BattlecraftItemsInvolved = $EventItemCsv->at($BattleLeveCsv->at($leve['DataId'])["ItemsInvolved[$i]"])['Name'];
+                        //$BNpcName = ucwords(strtolower($BNpcNameCsv->at($BattleLeveCsv->at($leve['DataId'])["BNpcName[$i]"])['Singular']));
+                        //$MobInvolvement[] = $BNpcName;
+                        //$InvolvementObjective[0] = "*Obtain target items.";
+                        //$InvolvementObjective[] = $BattlecraftItemsInvolved;
 
-            $NpcInvolvement = false;
-            $Npc = false;
+                    //}
+                }
+            } // Need to do something for fieldcraft ones, but haven't even begun to think about it yet so commenting out.
+            //elseif ($levetype == "Fieldcraft") { }
+
+            $MobInvolvement = array_unique($MobInvolvement);
+            $MobInvolvement = implode(", ", $MobInvolvement);
+            $InvolvementObjective = array_unique($InvolvementObjective);
+            $MobObjective = implode("\n*", $InvolvementObjective);
 
                 // Save some data
             $data = [
+                '{Top}' => $Top,
                 '{patch}' => $patch,
                 '{index}' => $leve['id'],
                 '{name}' => $leve['Name'],
                 '{level}' => $leve['ClassJobLevel'],
                 '{guildtype}' => $guildtype[$leve['LeveVfx']],
+                '{duration}' => ($levetype == "Battlecraft") ? "\n|Leve Duration      = ". $leve['TimeLimit'] ."m" : "",
                 '{levetype}' => $levetype,
                 '{grandcompany}' => ($leve['LeveAssignmentType'] == 16 || $leve['LeveAssignmentType'] == 17 || $leve['LeveAssignmentType'] == 18)
                     ? "\n|Grand Company      = ". $grandcompany : "",
                 '{classes}' => $classes,
-                '{objective}' => $Objective,
+                '{objective}' => $TradecraftObjective,
+                '{mobobjective}' => $MobObjective,
                 '{description}' => $leve['Description'],
                 '{exp}' => ($leve['ExpReward'] > 0) ? $leve['ExpReward'] : "{{Information Needed}}",
                 '{gil}' => $leve['GilReward'],
                 '{npc}' => $Npc,
                 '{client}' => $LeveClientCsv->at($leve['LeveClient'])['Name'],
                 '{npcinvolve}' => $NpcInvolvement,
+                '{mobinvolve}' => $MobInvolvement,
                 '{item}' => $Item,
+                '{Bottom}' => $Bottom,
             ];
 
             // format using Gamer Escape formatter and add to data array
