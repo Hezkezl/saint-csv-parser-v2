4ed5d3a2b5906d0f72360f915312e307778386f6
Icarus <36118925+IcarusTwine@users.noreply.github.com>
2020-05-19 23:16:26 +0100

    not fixed yet but latest work to (line 364)

--- src/Parsers/GE/Leves.php
+++ src/Parsers/GE/Leves.php
@@ -298,6 +298,7 @@ class Leves implements ParseInterface
                         $BCItemsInvolved = false;
                         $BCItemQTY = false;
                         $BCItemDropRate = false;
+                        $ItemIF = false;
                         $ItemIFSingular = false;
 
                         $TargetNumber = ($TargetNumber + 1);
@@ -360,6 +361,8 @@ class Leves implements ParseInterface
                         $MobObjectiveList[0] = "\n|Objectives =\n*" . $ObjectiveText;
                         if ($BattleLeveCsv->at($leve['DataId'])["ToDoNumberInvolved[$i]"] > 0) {
                             $MobObjectiveList[] = "**" . $MobsInvolved . ": 0/" . $BattleLeveCsv->at($leve['DataId'])["ToDoNumberInvolved[$i]"] . "";
+                        } elseif ($BattleLeveCsv->at($leve['DataId'])["ToDoNumberInvolved[$i]"] == 0) {
+                            $MobObjectiveList[] = "**" . $EventItemCsv->at($BattleLeveCsv->at($leve['DataId'])["ItemsInvolved[$i]"])['Name'] . ": 0/" . $BattleLeveCsv->at($leve['DataId'])["ToDoParam[$i]"] . "";
                         }
 
                         //$BattleObjective = "\n|Objectives = \n*" . $ObjectiveText;
