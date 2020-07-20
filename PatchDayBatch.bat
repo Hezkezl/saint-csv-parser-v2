6e658361951cfd422ccdb496507c78d262dea1f7
Icarus <36118925+IcarusTwine@users.noreply.github.com>
2020-07-20 18:13:27 +0100

    Update PatchDayBatch.bat
    Make sure you fix paths for

--- PatchDayBatch.bat
+++ PatchDayBatch.bat
@@ -12,4 +12,23 @@ php bin/console app:parse:csv GE:Satisfaction
 php bin/console app:parse:csv GE:SpecialShop
 php bin/console app:parse:csv GE:TripleTriad
 Echo All files finished and up to current date defined in gamever.
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Would you like to run WinMerge on this? (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+rem location of WinMerge
+set c="PATH TO WINMERGE.EXE"
+
+set d="PATH TO PARSER OUTPUT FOLDER"
+rem example: E:\saint-csv-parser-v2-master\output
+cd $c%
+
+rem old -  new
+set /p "a=old patch number: "
+set /p "b=new patch number: "
+WinMergeU /r "%d%\%a%\" "%d%\%b%"
+
+:END
+endlocal
 pause
