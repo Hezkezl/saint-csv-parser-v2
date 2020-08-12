afba1dd6456456b06ccf8bbf18539339773b6308
Hezkezl <raistlindlleader@hotmail.com>
2020-08-12 14:08:28 -0600

    Added icon copying for Achievements
    plus minor spacing cleanup

--- src/Parsers/GE/Achievement.php
+++ src/Parsers/GE/Achievement.php
@@ -60,28 +60,27 @@ class Achievement implements ParseInterface
             // manually, set to false
             $Bot = "true";
 
-            $index = $Achievement['id'];
-            $name = $Achievement['Name'];
             $descriptionPre = $Achievement['Description'];
             $oldwords = ["gladiator", "pugilist", "marauder", "lancer", "archer", "conjurer", "thaumaturge", "carpenter", "blacksmith", "armorer", "goldsmith", "leatherworker", "weaver", "alchemist", "culinarian", "miner", "botanist", "fisher", "paladin", "monk", "monk", "dragoon", "bard", "white mage", "black mage", "arcanist", "summoner", "scholar", "rogue", "ninja", "machinist", "dark knight", "astrologian", "samurai", "black mage", "red mage", "blue mage", "gunbreaker", "dancer"];
-            $newwords = ["[[Gladiator]]","[[Pugilist]]","[[Marauder]]","[[Lancer]]","[[Archer]]","[[Conjurer]]","[[Thaumaturge]]","[[Carpenter]]","[[Blacksmith]]","[[Armorer]]","[[Goldsmith]]","[[Leatherworker]]","[[Weaver]]","[[Alchemist]]","[[Culinarian]]","[[Miner]]","[[Botanist]]","[[Fisher]]","[[Paladin]]","[[Monk]]","[[Warrior]]","[[Dragoon]]","[[Bard]]","[[White Mage]]","[[Black Mage]]","[[Arcanist]]","[[Summoner]]","[[Scholar]]","[[Rogue]]","[[Ninja]]","[[Machinist]]","[[Dark Knight]]","[[Astrologian]]","[[Samurai]]","[[Black Mage]]","[[Red Mage]]","[[Blue Mage]]","[[Gunbreaker]]","[[Dancer]]"];
+            $newwords = ["[[Gladiator]]", "[[Pugilist]]", "[[Marauder]]", "[[Lancer]]", "[[Archer]]", "[[Conjurer]]", "[[Thaumaturge]]", "[[Carpenter]]", "[[Blacksmith]]", "[[Armorer]]", "[[Goldsmith]]", "[[Leatherworker]]", "[[Weaver]]", "[[Alchemist]]", "[[Culinarian]]", "[[Miner]]", "[[Botanist]]", "[[Fisher]]", "[[Paladin]]", "[[Monk]]", "[[Warrior]]", "[[Dragoon]]", "[[Bard]]", "[[White Mage]]", "[[Black Mage]]", "[[Arcanist]]", "[[Summoner]]", "[[Scholar]]", "[[Rogue]]", "[[Ninja]]", "[[Machinist]]", "[[Dark Knight]]", "[[Astrologian]]", "[[Samurai]]", "[[Black Mage]]", "[[Red Mage]]", "[[Blue Mage]]", "[[Gunbreaker]]", "[[Dancer]]"];
             $description = str_replace($oldwords, $newwords, $descriptionPre);
-            //if its a type 6 then add the link to the desciption for quests
+            //if its a type 6 then add the link to the description for quests
             if ($Achievement['Type'] == 6) {
                 $questkeyname = $QuestCsv->at($Achievement["Key"])['Name'];
                 $description = str_ireplace($questkeyname, "[[". $questkeyname ."]]", $description);
-                foreach(range(0,7) as $i) {
+                foreach (range(0, 7) as $i) {
                     $questname = $QuestCsv->at($Achievement["Data[$i]"])['Name'];
                     $description = str_ireplace($questname, "[[". $questname ."]]", $description);
                 }
             }
+
             //if its a type 2 change description like above then add the link for required achievement
             $reqachievement = [];
             if ($Achievement['Type'] == 2) {
                 $reqachievement[0] = "". $AchievementCsv->at($Achievement["Key"])['Name'] ."";
                 $reqachikeyname = $AchievementCsv->at($Achievement["Key"])['Name'];
                 $description = str_ireplace($reqachikeyname, "[[". $reqachikeyname ."]]", $description);
-                foreach(range(0,7) as $i) {
+                foreach (range(0, 7) as $i) {
                     $reqachiname = $AchievementCsv->at($Achievement["Data[$i]"])['Name'];
                     $description = str_ireplace($reqachiname, "[[". $reqachiname ."]]", $description);
                     if (!empty($AchievementCsv->at($Achievement["Data[$i]"])['Name'])) {
@@ -89,28 +88,29 @@ class Achievement implements ParseInterface
                     }
                 }
             }
-            //
+
             $reqachievement = implode(" ,", $reqachievement);
             //if its a type 8 then replace the zone in description with the link
             if ($Achievement['Type'] == 8) {
                 $keyname = $PlaceNameCsv->at($MapCsv->at($Achievement["Key"])['PlaceName'])['Name'];
                 $description = str_ireplace($keyname, "[[". $keyname ."]]", $description);
             }
-            $points = $Achievement['Points'];
+
             //sets title to nothing is there is no reward
-            $titleMale = "";
-            $titleFemale = "";
+            $titleMale = false;
+            $titleFemale = false;
             if ($Achievement['Title'] != "0") {
                 $titleMale = "". $TitleCsv->at($Achievement['Title'])['Masculine'] ."";
                 $titleFemale = "". $TitleCsv->at($Achievement['Title'])['Feminine'] ."";
             }
-            $item = $ItemCsv->at($Achievement['Item'])['Name'];
+
             //puts the correct amount of 00 before the icon id
             $icon = sprintf("%06d", $Achievement['Icon']);
 
+            $name = $Achievement['Name'];
+            $item = $ItemCsv->at($Achievement['Item'])['Name'];
             $type = $AchievementCategoryCsv->at($Achievement['AchievementCategory'])['Name'];
             $category = $AchievementKindCsv->at($AchievementCategoryCsv->at($Achievement['AchievementCategory'])['AchievementKind'])['Name'];
-
 
             // change the top and bottom code depending on if I want to bot the pages up or not. Places Patch on subpage
             if ($Bot == "true") {
@@ -121,15 +121,45 @@ class Achievement implements ParseInterface
                 $Bottom = "";
             };
 
+            //quest header image copying code. Should probably comment this out most of the time with /* before
+            //the beginning of the code and put */ after the code for easier commenting, as compared to
+            //putting // in front of every line. ie:  */ commented out code here <line breaks etc/everything too> /*
+
+            if (!empty($Achievement['Icon'])) {
+                if (!file_exists($this->getOutputFolder() ."/$CurrentPatchOutput/AchievementIcons/$icon.png")) {
+                    // ensure output directory exists
+                    $IconOutputDirectory = $this->getOutputFolder() ."/$CurrentPatchOutput/AchievementIcons";
+                    if (!is_dir($IconOutputDirectory)) {
+                        mkdir($IconOutputDirectory, 0777, true);
+                    }
+
+                    // build icon input folder paths
+                    $InputIcon = $this->getInputFolder() . '/icon/' . $this->iconize($icon);
+
+                    // if icon doesn't exist (not in the input folder icon list), then skip
+                    //if (!file_exists($questIcon)) continue;
+
+                    $IconFileName = "{$IconOutputDirectory}/{$icon}.png";
+
+                    // inform console what item we're copying
+                    //$this->io->text("Ability: <comment>{$quest['Name']}</comment>");
+                    //$this->io->text(
+                    //sprintf('- copy <info>%s</info> to <info>%s</info>', $questIcon, $questiconFileName));
+
+                    // copy the input icon to the output filename
+                    copy($InputIcon, $IconFileName);
+                }
+            }
+
             // Save some data
             $data = [
                 '{Top}' => $Top,
-                '{index}' => $index,
+                '{index}' => $Achievement['id'],
                 '{patch}' => $Patch,
                 '{name}' => $name,
                 '{item}' => $item,
                 '{icon}' => $icon,
-                '{points}' => $points,
+                '{points}' => $Achievement['Points'],
                 '{description}' => $description,
                 '{titleMale}' => $titleMale,
                 '{titleFemale}' => $titleFemale,
