c1da8a1a516db0f2040fbe46af21b4e1ce1ab580
Hezkezl <raistlindlleader@hotmail.com>
2020-08-11 22:54:41 -0600

    Update GeFormatter.php
    added regex cleanup for > Quest Sync in description. Also consolidated some of the 'find X replace with null' regex search

--- src/Parsers/GE/GeFormatter.php
+++ src/Parsers/GE/GeFormatter.php
@@ -17,13 +17,10 @@ class GeFormatter
         $format = str_ireplace("        \n","\n", $format);
         $format = str_ireplace('        }}','}}', $format);
         $format = str_ireplace('        {{','{{', $format);
-        $format = preg_replace("/(.*)dammy(.*)\n/", null, $format);
-        $format = preg_replace("/(.*)★未使用(.*)削除予定★(.*)\n/",null, $format);
-        $format = preg_replace("/{{Loremquote\\|Todo\d\d+\\|link=y\\|(.*)\n/", null, $format);
-        //$format = preg_replace("/\n\n\n+/", "\n\n", $format);
+        //null replacement regex (find something, replace with nothing)
+        $format = preg_replace("/(.*)dammy(.*)\n|(.*)★未使用(.*)削除予定★(.*)\n|{{Loremquote\\|Todo\d\d+\\|link=y\\|(.*)\n|\s*|\s*|<UIForeground>F201FC<\/UIForeground><UIGlow>\w+<\/UIGlow> Quest Sync<UIGlow>\d+<\/UIGlow><UIForeground>\d+</UIForeground>\n/", null, $format);
         $format = preg_replace("/(QuestReward.*)\n\n(?!\\|Issuing NPC)/", "$1\n", $format);
         $format = preg_replace("/(QuestReward.*)\n(\\|Issuing NPC.*)/", "$1\n\n$2", $format);
-        $format = preg_replace("/\s*|\s*/", null, $format);
         $format = preg_replace("/<Emphasis>|<\\/Emphasis>/", "''", $format);
         $format = preg_replace("/<If\\(LessThan\\(PlayerParameter\\(11\\),12\\)\\)><If\\(LessThan\\(PlayerParameter\\(11\\),4\\)\\)>([^>]+)<Else\\/>([^>]+)<\\/If><Else\\/><If\\(LessThan\\(PlayerParameter\\(11\\),17\\)\\)>([^>]+)<Else\\/>([^>]+)<\\/If><\\/If>/", "{{Loremtextconditional|$1|or '$2' or '$3', depending on the time of day.}}", $format);
         $format = preg_replace("/{{Loremquote\\|Q\d+\\|link=y\\|(.*)}}/","\n{| class=\"datatable-GEtable\"\n|+$1\n|Place an answer Here <!--(Not all questions have answers and thus don't need a table, please evaluate and delete this if necessary.)-->\n|}\n", $format);
