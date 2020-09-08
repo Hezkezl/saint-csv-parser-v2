2f12f20783fd03b042a60230b7845b4c4f1fc2c1
Hezkezl <raistlindlleader@hotmail.com>
2020-09-08 06:05:37 -0600

    Update SmallIconConverter.php
    replace spaces with _'s
    also replace / in names with a - (fuck you "Torn from the Heavens/The Dark Colossus blahblah Orchestrion Roll")
    also added in temp code to skip copying if the item ID didn't match what was in an array

--- src/Parsers/GE/SmallIconConverter.php
+++ src/Parsers/GE/SmallIconConverter.php
@@ -17,7 +17,7 @@ class SmallIconConverter implements ParseInterface
 
     public function parse()
     {
-        include (dirname(__DIR__) . '/Paths.php');
+        include(dirname(__DIR__) . '/Paths.php');
 
         // ensure output directory exists
         $outputDirectory = $this->getOutputFolder() . "/$CurrentPatchOutput/40pxitemicons";
@@ -29,17 +29,25 @@ class SmallIconConverter implements ParseInterface
         $ItemCsv = $this->csv('Item');
 
         // loop through data
-        foreach($ItemCsv->data as $id => $item)
-        {
+        foreach ($ItemCsv->data as $id => $item) {
             // Skip shit with no name or icon
-            $item['Name']  = strip_tags($item['Name']);
             if (!$item['Name'] || !$item['Icon']) {
                 continue;
             }
+            //only run this code if you need to get specific item ID#'s for icon copying.
+            //$skip = array("30413", "30414", "30415", "30416", "30417", "30418", "30762", "30763", "30764", "30765", "30766", "30862", "30871", "30875", "31184", "31185", "31186");
+            //Also need to add a new closing } at the very bottom of the file.
+            //if (in_array($id, $skip)) {
+            //Alternate code: the next line, OR the two after it
+            //if (in_array('$item["id"]', $skip, FALSE)) continue;
+            //$variable = FALSE;
+            //if (in_array('$item["id"]', $skip, $variable)) continue;
+
+            $item['Name'] = strip_tags($item['Name']);
 
             // build icon and hq icon input folder paths
             $itemIcon = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon']);
-            $itemIconHq = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon'], true);
+            //$itemIconHq = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon'], true);
 
             // if icon doesn't exist (not in the input folder icon list), then skip
             if (!file_exists($itemIcon)) {
@@ -50,8 +58,10 @@ class SmallIconConverter implements ParseInterface
             $this->io->text("Item: <comment>{$item['Name']}</comment>");
 
             // build output filenames for icon + hq icon
-            $iconFileName = "{$outputDirectory}/{$item['Name']}_Icon.png";
-            $iconFileNameHq = "{$outputDirectory}/{$item['Name']}_HQ_Icon.png";
+            // replace spaces with underscores, also replace any / in names with a -
+            // (mainly for "Torn from the Heavens/The Dark Colossus orchestrion roll")
+            $iconFileName = "{$outputDirectory}/". str_replace(" ", "_", str_replace("/", "-", $item['Name'])) ."_Icon.png";
+            //$iconFileNameHq = "{$outputDirectory}/". str_replace(" ", "_", $item['Name']) ."_HQ_Icon.png";
 
             // console output
             $this->io->text(
