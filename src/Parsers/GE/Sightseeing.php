773d1d8460cede2f1b08fc8f20c69f24389b9001
Hezkezl <raistlindlleader@hotmail.com>
2020-12-22 04:35:00 -0700

    Partial update to Sightseeing.php
    added Expansion check based off of ID#
    Fixed Vista Record Number based off of ID (was previously only using one calculation for the most recent set of locations)
    Still partially broken though. This only spits out 059-062 Stormblood logs, and then all of SHB. It skips ARR, HW, and 001-058 SB for some reason...

--- src/Parsers/GE/Sightseeing.php
+++ src/Parsers/GE/Sightseeing.php
@@ -16,12 +16,11 @@ class Sightseeing implements ParseInterface
     const WIKI_FORMAT = "{{-start-}}
 '''Sightseeing Log: {name}'''
 {{ARR Infobox Sightseeing Log
-| Patch = {patch}
-| Expansion   = Shadowbringers
+| Patch = {patch}{expansion}
 | Name        = {name}
 | Location    = {name}
 | Coordinates = {{Information Needed}}
-| Vista Record Number = 0{number}
+| Vista Record Number = {number}
 | Impression = \"{impression}\"
 
 | Description = {description}
@@ -37,7 +36,7 @@ class Sightseeing implements ParseInterface
 | Screenshot =
 | Screenshot Description =
 
-| Vista Image = Sightseeing Log - SHB0{number}-Complete.png
+| Vista Image = {vista}
 | Vista Image Description =
 
 | Icon = {name} Image.png
@@ -57,37 +56,68 @@ class Sightseeing implements ParseInterface
         foreach ($AdventureCsv->data as $id => $item) {
             $this->io->progressAdvance();
 
-            $name2 = str_replace("<Emphasis>", "", $item['Name']);
-            $name = str_replace("</Emphasis>", "", $name2);
+            $name = preg_replace("/\<Emphasis>|<\/Emphasis>/", null, $item['Name']);
             $emote = $EmoteCsv->at($item['Emote'])['Name'];
-            $number = ($id-2162687-204);
 
+            // SS log number code. Needs these calculations to properly show 001, 002, 015, etc
+            // $expansionshort is used to show the abbreviation of the expansion name (HW, SB, SHB) for the image name
+            $expansion = false;
+            $expansionshort = false;
+            if ($id <= 2162767) {
+                $number1 = $id-2162687;
+            } elseif (($id >= 2162768) && ($id <= 2162829)) {
+                $number1 = $id-2162767;
+                $expansion = "\n| Expansion   = Heavensward";
+                $expansionshort = "HW";
+            } elseif (($id >= 2162830) && $id <= 2162891) {
+                $number1 = $id-2162829;
+                $expansion = "\n| Expansion   = Stormblood";
+                $expansionshort = "SB";
+            } elseif (($id >= 2162892) && $id <= 2162936) {
+                $number1 = $id-2162891;
+                $expansion = "\n| Expansion   = Shadowbringers";
+                $expansionshort = "SHB";
+            } else {
+                $number1 = "\n| Expansion   = ERROR: INVALID SIGHTSEEING LOG NUMBER";
+            }
+            // ensure there are always leading 0s. Two if single digit, one if double digit
+            $number = str_pad($number1, 3, "0", STR_PAD_LEFT);
+
+            // Vista Image name (used in icon copying section as well as in template above)
+            $Vista = "Sightseeing Log - {$expansionshort}{$number}-Complete.png";
+
+            // icon copying code
             // ensure output directory exists
-            $IconoutputDirectory = $this->getOutputFolder() ."/$CurrentPatchOutput/SightseeingLogIcons";
-            if (!is_dir($IconoutputDirectory)) {
-                mkdir($IconoutputDirectory, 0777, true);
+            $smallIconOutputDirectory = $this->getOutputFolder() ."/$CurrentPatchOutput/SightseeingLogIcons/small";
+            $largeIconOutputDirectory = $this->getOutputFolder() ."/$CurrentPatchOutput/SightseeingLogIcons/large";
+            if (!is_dir($smallIconOutputDirectory)) {
+                mkdir($smallIconOutputDirectory, 0777, true);
+            }
+            if (!is_dir($largeIconOutputDirectory)) {
+                mkdir($largeIconOutputDirectory, 0777, true);
             }
 
             // build icon input folder paths
-            $itemIconsmall = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon{List}']);
-            $itemIconlarge = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon{Discovered}']);
+            $smallIcon = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon{List}']);
+            $largeIcon = $this->getInputFolder() .'/icon/'. $this->iconize($item['Icon{Discovered}']);
+            $smalliconFileName = "{$smallIconOutputDirectory}/{$name} SS Icon.png";
+            $largeiconFileName = "{$largeIconOutputDirectory}/{$Vista}";
 
-            $iconFileName = "$IconoutputDirectory/small/{$name} Image.png";
-            $iconFileNameLarge = "$IconoutputDirectory/large/Sightseeing Log - SHB0{$number}-Complete.png";
-
-            // copy the input icon to the output filename
-            copy($itemIconsmall, $iconFileName);
-            copy($itemIconlarge, $iconFileNameLarge);
+            // copy the input icon to the output filenames
+            copy($smallIcon, $smalliconFileName);
+            copy($largeIcon, $largeiconFileName);
 
             // Save some data
             $data = [
                 '{patch}' => $Patch,
                 '{name}' => $name,
+                '{expansion}' => $expansion,
                 '{emote}' => $emote,
                 '{impression}' => $item['Impression'],
                 '{description}' => $item['Description'],
                 '{number}' => $number,
                 '{time}' => ($item['MinTime'] > 0) ? " {$item['MinTime']}-{$item['MaxTime']}" : '',
+                '{vista}' => $Vista,
             ];
 
             // format using Gamer Escape formatter and add to data array
