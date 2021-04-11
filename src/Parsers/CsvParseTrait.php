85156d417ca507acb464dbfd3ea67ce27dc5b87a
Icarus <36118925+IcarusTwine@users.noreply.github.com>
2021-04-11 18:57:11 +0100

    Update CsvParseTrait.php

--- src/Parsers/CsvParseTrait.php
+++ src/Parsers/CsvParseTrait.php
@@ -1164,4 +1164,32 @@ trait CsvParseTrait
 
         return $icon;
     }
+
+    
+    /**
+     * Converts SE icon "number" into a proper path for /EN 
+     */
+    private function iconizeEN($number, $hq = false)
+    {
+        $number = intval($number);
+        $extended = (strlen($number) >= 6);
+
+        if ($number == 0) {
+            return null;
+        }
+
+        // create icon filename
+        $icon = $extended ? str_pad($number, 5, "0", STR_PAD_LEFT) : '0' . str_pad($number, 5, "0", STR_PAD_LEFT);
+
+        // create icon path
+        $path = [];
+        $path[] = $extended ? $icon[0] . $icon[1] . $icon[2] .'000' : '0'. $icon[1] . $icon[2] .'000';
+
+        $path[] = $icon;
+
+        // combine
+        $icon = implode('/en/', $path) .'.png';
+
+        return $icon;
+    }
 }
